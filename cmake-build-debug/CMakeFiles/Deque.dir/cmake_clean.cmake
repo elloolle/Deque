31be01e70c4e411a3000aa003ab1c8b1fad388c6
Hello on loptop <coollemon1000.com>
2024-12-17 16:46:39 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Deque.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Deque.dir/main.cpp.o"
+  "CMakeFiles/Deque.dir/main.cpp.o.d"
+  "Deque"
+  "Deque.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Deque.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
